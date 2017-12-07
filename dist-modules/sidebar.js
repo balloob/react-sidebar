70eaf57b6d90ab24f9fb49ff1c3c83f064b9a708
Andrey Luiz <andreyluiz.developer@gmail.com>
2017-12-07 16:51:39 -0200

    Fix warning.

--- dist-modules/sidebar.js
+++ dist-modules/sidebar.js
@@ -282,12 +282,12 @@ var Sidebar = function (_Component) {
         sidebarStyle.right = 0;
         sidebarStyle.transform = 'translateX(100%)';
         sidebarStyle.WebkitTransform = 'translateX(100%)';
-        sidebarStyle.boxShadow = this.props.shadow || '-2px 2px 4px rgba(0, 0, 0, 0.15)';
+        sidebarStyle.boxShadow = this.props.shadow || 'rgba(0, 0, 0, 0.15) -2px 2px 4px';
       } else {
         sidebarStyle.left = 0;
         sidebarStyle.transform = 'translateX(-100%)';
         sidebarStyle.WebkitTransform = 'translateX(-100%)';
-        sidebarStyle.boxShadow = this.props.shadow || '2px 2px 4px rgba(0, 0, 0, 0.15)';
+        sidebarStyle.boxShadow = this.props.shadow || 'rgba(0, 0, 0, 0.15) 2px 2px 4px';
       }
 
       if (isTouching) {
