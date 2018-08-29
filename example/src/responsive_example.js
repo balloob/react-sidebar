d6efbd1273a8b9d591a6ff5c8d7e7c61478c34b2
Germán Ruelas <lgruelas@hotmail.com>
2018-08-29 00:04:00 -0500

    Fix import statements for a succesfull build in Travis

--- example/src/responsive_example.js
+++ example/src/responsive_example.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Sidebar from "../..";
+import Sidebar from "../../src/sidebar";
 import MaterialTitlePanel from "./material_title_panel";
 import SidebarContent from "./sidebar_content";
 
